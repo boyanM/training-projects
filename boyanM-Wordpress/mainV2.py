641397c7407114574bb3e193d38dc80ed4bf4450
boyanM <boyan1044@gmail.com>
2019-09-12 12:16:41 +0300

    Fiinished version

--- boyanM-Wordpress/mainV2.py
+++ boyanM-Wordpress/mainV2.py
@@ -1,61 +1,120 @@
 #!/usr/bin/python3
 
+#Code Style - camelCase
+#Author - Boyan Milanov
+
 import requests
 import json
-
-
-
+import psycopg2
+import time
 
 def request(count,bbox):
 	#Example: ?bbox=22,41,34,43,1000&appid=4d90eceaefb65097f2a3d9f86539e3f5
 	url = 'https://api.openweathermap.org/data/2.5/box/city'
-	bbox = str(lngBot)+","+str(latBot)+","+str(lngTop)+","+str(latTop)+","+"1000"
 	payload = {'bbox':bbox,'appid':'4d90eceaefb65097f2a3d9f86539e3f5'}
 	r = requests.get(url,params=payload)
 	r_dict = r.json()
 	if len(r_dict) == 0:
-		neededinfo = [count,'']
-		return neededinfo
+		return (count,'')
 	else:
 		string=''
 		listOfAllCities = r_dict['list']
-		print(listOfAllCities[0]['name'])
-		print(listOfAllCities[0]['id'])
 		coordinates_dict = listOfAllCities[0]['coord']
-		print(coordinates_dict['Lat'])
-		print(coordinates_dict['Lon'])
-		#name = name of the city
-		#id = id of the city
-		#coord = coordinates of the city
-	
 		for element in range(len(listOfAllCities)):
 			coordinates_dict = listOfAllCities[element]['coord']
 			coordinates = ''
 			coordinates = ","+str(coordinates_dict['Lat']) + "," + str(coordinates_dict['Lon']) + '\n'
 			string += str(count) + ","+str(listOfAllCities[element]['id'])
-			string +=","+listOfAllCities[element]['name']
+			string +=","+ "'" + listOfAllCities[element]['name'] + "'"
 			string += coordinates
 			count += 1
-		neededinfo = []
-		neededinfo.append(count)
-		neededinfo.append(string)	
-		return neededinfo
+			
+		return (count,string)
 
+def geoCalc(lngBot,latBot,lngTop,latTop):
+	localArea = []
+	if lngBot > lngTop:
+		tmp = lngBot
+		lngBot = lngTop
+		lngTop = tmp
+		del tmp
 
+	if latBot > latTop:
+		tmp = latBot
+		latBot = latTop
+		latTop = tmp
+		del tmp
 
-markers =""
+	for lng in range(lngBot,lngTop-7,8):
+		for lat in range(latBot,latTop-2,3):
+			localArea.append((lng,lat,lng+8,lat+3)) #Tuple (Lon Bottom,Lat Bottom, Lon Top, Lat Top)
+	return localArea
+			
 count = 1
+markers = ""
+
+area = []
+
+#South America
+SA = geoCalc(-71,-17,-50,13)
 
 #Europe
-for lng in range(-9,41,8):
-	for lat in range(37,68,3):
-		lngBot = lng
-		latBot = lat
-		lngTop = lng + 8
-		latTop = lat + 3
-		bbox = str(lngBot) + "," + str(latBot) + "," + str(lngTop) + "," + str(latTop) + ",1000" 
-		info = request(count,bbox)
-		markers += info[1]
-		count = info[0]
-		
+EU = geoCalc(-9,37,47,70)
+
+#Wyoming - U.S.
+WY = geoCalc(-111,40,-103,45)
+
+#From Arkansas to New York
+A_NY = geoCalc(-93,33,-70,43)
+
+#Asia
+AS = geoCalc(8,77,134,46)
+
+area.extend(SA)
+area.extend(EU)
+area.extend(WY)
+area.extend(A_NY)
+area.extend(AS)
+
+print("Number of Cities: ",len(area))
+
+start = time.time()
+for i in range(len(area)):
+	bbox = str(area[i][0]) + "," + str(area[i][1]) + "," + str(area[i][2])
+	bbox += "," + str(area[i][3]) + ",1000" 
+	info = request(count,bbox)
+	count = info[0]
+	markers += info[1]
 print(markers)
+end = time.time()
+alltime = end - start
+print("Transfer from API time: ",alltime)
+start = time.time()
+
+try:
+	connection = psycopg2.connect(dbname = "wordpress",
+		user = "wpuser",
+		password = "password",
+		port="5432",
+		host="127.0.0.1")
+	cursor = connection.cursor()
+	cursor.execute("select count(id) from markers; ")
+	empty = cursor.fetchone()
+	if empty[0] != 0:
+		cursor.execute("truncate markers")
+	lines = markers.splitlines()
+	for i in range(len(lines)):
+		insert = "insert into markers(id,number,country,lat,lng) values (" + lines[i] + ");"
+		cursor.execute(insert)
+		connection.commit()
+except (Exception, psycopg2.Error) as error :
+	print ("Error while connecting to PostgreSQL", error)
+finally:
+	if(connection):
+		cursor.close()
+		connection.close()
+		print("Connection to PostgreSQL is closed")
+
+end = time.time()
+alltime = end - start
+print("Transfer data to PostgreSQL time : ",alltime)
