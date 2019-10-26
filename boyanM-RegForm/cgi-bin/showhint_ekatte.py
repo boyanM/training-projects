b4ee6df2bd183dd735134b729d5faddb5b432dea
Boyan Milanov <33575839+boyanM@users.noreply.github.com>
2019-10-26 19:30:34 +0300

    Update showhint_ekatte.py

--- boyanM-RegForm/cgi-bin/showhint_ekatte.py
+++ boyanM-RegForm/cgi-bin/showhint_ekatte.py
@@ -8,8 +8,13 @@ import json
 form = cgi.FieldStorage()
 
 settlement = form['q'].value
-print("""Content-type:text/html\r\n\r\n
-<html>""")
+print("""Content-type:text/html charset:utf-8\r\n\r\n
+<html>
+<head>
+<meta charset=utf-8>
+</head>
+""")
+
 try:
 	connection = psycopg2.connect(dbname="ekatte",
 	user="ekatte_read",
@@ -23,10 +28,12 @@ try:
 	 where lower(s.name) like concat(lower(%s),%s)\
 	 and s.township_id = t.id and t.area_id=a.id limit 5;",(settlement,'%',))
 	result = cursor.fetchall()
+
 	print('<datalist id="ekatte">')
 	for i in range(len(result)):
-		print('<option value="%s">обл. %s</option>'%("[%s] %s"
-			%(result[i][0],result[i][1]),result[i][2]))
+		part1 = "[%s] %s"%(result[i][0],result[i][1])
+		part2 = result[i][2]
+		print('<option value="%s">обл. %s</option>'%(part1,part2))
 	print('</datalist>')
 
 except (Exception,psycopg2.Error) as error:
